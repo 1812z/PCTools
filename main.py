af4059488f4e043d7b83c18d184df69a9e98f5a0
1812z <61372168+1812z@users.noreply.github.com>
2024-04-28 20:35:54 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import paho.mqtt.client as mqtt
+import os
+import subprocess
+
+#mqtt服务器信息
+broker = 'bemfa.com'
+topic = 'dnkz005'
+secret_id = '你的id'
+port = 9501
+
+def on_subscribe(client, userdata, mid, reason_code_list, properties):
+    for sub_result in reason_code_list:
+        if sub_result >= 128:
+            print("Subscribe failed")
+
+def on_unsubscribe(client, userdata, mid, reason_code_list, properties):
+    if len(reason_code_list) == 0 or not reason_code_list[0].is_failure:
+        print("unsubscribe succeeded (if SUBACK is received in MQTTv3 it success)")
+    else:
+        print(f"Broker replied with failure: {reason_code_list[0]}")
+    client.disconnect()
+
+def on_message(client, userdata, message):
+    userdata.append(message.payload)
+    command = message.payload.decode()
+    print(f"Received `{command}` from `{message.topic}` topic")
+    #这里放置需要运行的命令
+    if(command == 'off'):
+        os.system("shutdown -s now ")
+    if(command == 'on#1') :
+        print("Run 'run.bat'")
+        os.system(r"C:\Users\i\Desktop\Coding\xiaoai_remote\run.bat")
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code.is_failure:
+        print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
+    else:
+        print("Connected to",broker)
+        client.subscribe(topic)
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+mqttc.user_data_set([])
+mqttc._client_id = secret_id
+mqttc.connect(broker,port)
+mqttc.loop_forever()
+print(f"Received the following message: {mqttc.user_data_get()}")
