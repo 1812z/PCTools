9f1ccece3145577fe1a3955037e460b14bdc5db1
1812z <61372168+1812z@users.noreply.github.com>
2024-05-26 12:34:04 +0800

    托盘管理

--- gui.py
+++ gui.py
@@ -1,3 +1,6 @@
+import threading
+import time
+from PIL import Image
 import flet as ft
 from aida64 import discovery,send_data
 from flet_core import MainAxisAlignment, CrossAxisAlignment
@@ -5,17 +8,18 @@ from web_task import FlaskAppManager
 from timer import start_task, stop_task
 import json
 from command import start_mqtt,stop_mqtt_loop
+import pystray
 
 version = "V1.0"
 manager = FlaskAppManager('192.168.44.236', 5000)
 run_flag = False
-
+page = None
 def save_json_data(key,data):
     json_data[key] = data
     with open('config.json', 'w', encoding='utf-8') as file:
         json.dump(json_data, file, indent=4)  
 
-def show_snackbar(page: ft.Page, message: str):
+def show_snackbar(page: ft.Page, message: str): 
     snackbar = ft.SnackBar(
         content=ft.Text(message),
         action="OK",
@@ -34,7 +38,14 @@ def start():
     if fun3:
         manager.start()
 
-def main(page: ft.Page):
+def close_windows():
+    if page:
+        page.window_destroy()
+
+def main(newpage: ft.Page):
+    global page
+    page = newpage
+
     page.window_width = 550
     page.window_height = 590
     page.title = "PCTools"
@@ -80,14 +91,18 @@ def main(page: ft.Page):
     def button_stop(e):
         global run_flag
         if run_flag == True:
+            show_snackbar(page,"停止进程中,请耐心等待..")
             if fun1:
                 stop_task()
+                print("停止监控反馈")
             if fun2:
                 stop_mqtt_loop()
+                print("停止远程命令")
             if fun3:
                 manager.stop()
+                print("停止画面传输")
             run_flag = False
-            show_snackbar(page,"停止进程...")  
+            show_snackbar(page,"已经停止进程")  
         else:
            show_snackbar(page,"都还没运行呀")
 
@@ -103,9 +118,18 @@ def main(page: ft.Page):
     def switch_fun4(e):
         fun4 = e.control.value
         save_json_data("fun4",fun4)
+    def input_user(e):
+        save_json_data("username",e.control.value)
+    def input_pass(e):
+        save_json_data("password",e.control.value)
+    def input_id(e):
+        save_json_data("secret_id",e.control.value)
+    def input_interval(e):
+        save_json_data("interval",int(e.control.value))
+       
+    def open_repo(e):
+        page.launch_url('https://github.com/1812z/PCTools')   
 
-        
-        
     home_page = [ft.Column(
             [
 
@@ -121,7 +145,12 @@ def main(page: ft.Page):
                         version+' by 1812z',
                         size=20,
                     ),
-                ),
+                ),ft.Row(
+                [
+                ft.Container(width=210),
+                ft.TextButton("Github",on_click=open_repo)
+                ]
+                 ),
                 ft.ElevatedButton(
                     content=ft.Row(
                         [
@@ -191,36 +220,38 @@ def main(page: ft.Page):
             horizontal_alignment=CrossAxisAlignment.CENTER, 
         ),ft.Row(
             [
-                ft.Container(width=210),
-                ft.Switch(
-                    label="监控反馈", label_position='rifht', scale=1.1,value=fun1,on_change=switch_fun1
-                ),
+                ft.Container(width=90)
+            ]
+        )
+        ,ft.Row(
+            [
+                ft.Container(width=90),
+                ft.Switch(label="监控反馈", label_position='left', scale=1.2,value=fun1,on_change=switch_fun1),
+                ft.Container(width=10),
+                ft.TextField(label="HA_MQTT账户", width=200,on_submit=input_user,value=read_user)
+            ]
+        ),ft.Row(
+            [
+                ft.Container(width=90),
+                ft.Switch(label="远程命令", label_position='left', scale=1.2,value=fun2,on_change=switch_fun2),
+                ft.Container(width=10),
+                ft.TextField(label="HA_MQTT密码", width=200,on_submit=input_pass,value=read_password)
             ]
            
         ),ft.Row(
             [
-                ft.Container(width=210),
-                ft.Switch(
-                    label="MQTT命令", label_position='rifht', scale=1.1,value=fun2,on_change=switch_fun2
-                ),
+                ft.Container(width=90),
+                ft.Switch(label="画面传输", label_position='left', scale=1.2,value=fun3,on_change=switch_fun3),
+                ft.Container(width=10),
+                ft.TextField(label="Secret_id", width=200,on_submit=input_id,value=read_secret_id)
             ]
            
         ),ft.Row(
             [
-                ft.Container(width=210),
-                ft.Switch(
-                    label="画面传输", label_position='rifht', scale=1.1,value=fun3,on_change=switch_fun3
-                ),
-            ]
-           
-        ),ft.Row(
-            [
-                ft.Container(width=210),
-                ft.Switch(
-                    label="自启动", label_position='rifht', scale=1.1,value=fun4,on_change=switch_fun4
-                ),
-                
-                
+                ft.Container(width=90),
+                ft.Switch(label="自动运行", label_position='left', scale=1.2,value=fun4,on_change=switch_fun4,tooltip="运行gui.py时自动运行勾选的服务"),
+                ft.Container(width=10),
+                ft.TextField(label="监控刷新间隔", width=200,on_submit=input_interval,value=read_interval)
             ]
            
         )
@@ -234,13 +265,53 @@ def main(page: ft.Page):
     # Update the page
     page.update()
 
+
+def on_exit(icon, item):
+    global icon_flag
+    if(run_flag == False):
+        icon_flag = False
+        close_windows()
+        icon.stop()
+    else:
+        icon.notify("服务正在运行无法关闭托盘")
+def show_menu(icon, item):
+    global show_menu_flag
+    show_menu_flag = 1
+
+# Run the icon in a separate thread to avoid blocking
+def icon_task():
+    image = Image.open("img/logo.png")
+    icon = pystray.Icon("test_icon")
+    icon.icon = image
+    icon.title = "PCTools"
+    icon.menu = pystray.Menu(
+        pystray.MenuItem("打开主菜单", show_menu),
+        pystray.MenuItem("退出托盘", on_exit)
+    )
+    icon.run()
+
+show_menu_flag = False
+icon_flag = True
 if __name__ == "__main__":
+    
+    threading.Thread(target=icon_task).start()
     with open('config.json', 'r') as file:
         json_data = json.load(file)
         fun1 = json_data.get("fun1")
         fun2 = json_data.get("fun2")
         fun3 = json_data.get("fun3")
         fun4 = json_data.get("fun4")
+        read_user = json_data.get("username")
+        read_password = json_data.get("password")
+        read_secret_id = json_data.get("secret_id")
+        read_interval = json_data.get("interval")
     if fun4:
+        run_flag = True
         start()
-    ft.app(target=main)
+    else:
+        ft.app(target=main)
+    while(icon_flag):
+        time.sleep(1)
+        if show_menu_flag:
+            show_menu_flag = False
+            ft.app(target=main)     
